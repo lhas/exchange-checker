0bd1cb8a3e0c654a1951697395868a0866a7c968
Luiz Henrique Almeida da Silva <luizhrqas@gmail.com>
2017-12-05 14:33:52 -0200

    displaying exchanges

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import {
-  Text,
   View
 } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Body, Text } from 'native-base';
+import axios from 'axios';
 
-const HomeScreen = ({ navigation }) => (
-  <View style={{ flex: 1 }}>
-    <Text>Home Screen</Text>
-  </View>
-);
+class HomeScreen extends React.Component {
+  state = {
+    exchanges: [],
+  };
+
+  componentDidMount() {
+    axios.get('https://api.bitvalor.com/v1/exchanges.json').then((response) => {
+      const exchanges = Object.keys(response.data).map(v => {
+        return response.data[v];
+      });
+      console.log(exchanges);
+
+      this.setState({
+        exchanges,
+      });
+    })
+  }
+  render() {
+    return (
+      <Container>
+        <Content>
+          {this.state.exchanges.map((exchange, k) => (
+          <Card key={k}>
+          <CardItem>
+            <Body>
+              <Text style={{ color: `#${exchange.color}` }}>
+                {exchange.name}
+                {exchange.url}
+                {exchange.url_book}
+              </Text>
+            </Body>
+          </CardItem>
+          </Card>
+          ))}
+        </Content>
+      </Container>
+    )
+  }
+}
 
 export default HomeScreen;
